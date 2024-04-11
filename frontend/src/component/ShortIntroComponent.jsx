cd21215f349277654e55d1b0007b6927b66f0050
Tseringdhoundup9813 <tseringlama9813694977@gmail.com>
2024-04-11 20:06:22 +0545

    small changes

--- frontend/src/component/ShortIntroComponent.jsx
+++ frontend/src/component/ShortIntroComponent.jsx
@@ -73,7 +73,7 @@ function ShortIntroComponent() {
              style={{originX:0}}
              transition={{delay:0.3,duration:0.5}}>
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+              Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
               when an unknown printer took a galley of type and scrambled it to make a type specimen book.
       </motion.p>
     </div>
