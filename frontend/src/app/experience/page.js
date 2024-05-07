02adfb5751d83f7eb6bf3cddf95eef0964190a2c
Tseringdhoundup9813 <tseringlama9813694977@gmail.com>
2024-05-07 22:55:50 +0545

    add experience context

--- frontend/src/app/experience/page.js
+++ frontend/src/app/experience/page.js
@@ -1,67 +1,89 @@
-
-"use client"
-import React from 'react'
-import '@/css/expe.css'
-import {motion} from "framer-motion"
-
-
+"use client";
+import React from "react";
+import "@/css/expe.css";
+import { motion } from "framer-motion";
 
 function ExperiencePage() {
   return (
-    <div className='experience-main-wrapper'>
-        <motion.h1 
-          initial={{x:150,opacity:0}}
-        animate={{x:0,opacity:1}}
-        transition={{duration:0.3}}
-        className='exp-main-title'>Expereince</motion.h1>
-        <motion.p className='exp-main-des'
-         initial={{x:-150,opacity:0}}
-         animate={{x:0,opacity:1}}
-         transition={{duration:0.3}}>
-        The experience that I have gained in recent years.</motion.p>
+    <div className="experience-main-wrapper">
+      <motion.h1
+        initial={{ x: 150, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.3 }}
+        className="exp-main-title"
+      >
+        Expereince
+      </motion.h1>
+      <motion.p
+        className="exp-main-des"
+        initial={{ x: -150, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.3 }}
+      >
+        The experience that I have gained in recent years.
+      </motion.p>
 
-        <div className='exp-box-wrapper'>
-            <div className='exp-box-container'>
-                <h1>Naksal Cholling Gompa</h1>
-                <p>Date 2024-3-10</p>
-                <p>survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                   It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-                  and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-                  <ul>
-                    <li>It is a long established fact that a reader will be distracted</li>
-                    <li>software like Aldus PageMaker including versions of Lorem Ipsum</li>
-                    <li>The standard chunk of Lorem Ipsum used</li>
-                  </ul>
-            </div>
-            {/* --------------- */}
-            <div className='exp-box-container'>
-                <h1>Naksal Cholling Gompa</h1>
-                <p>Date 2024-3-10</p>
-                <p>survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                   It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-                  and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-                  <ul>
-                    <li>It is a long established fact that a reader will be distracted</li>
-                    <li>software like Aldus PageMaker including versions of Lorem Ipsum</li>
-                    <li>The standard chunk of Lorem Ipsum used</li>
-                  </ul>
-            </div>
-            {/* ---------------- */}
-            <div className='exp-box-container'>
-                <h1>Naksal Cholling Gompa</h1>
-                <p>Date 2024-3-10</p>
-                <p>survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                   It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-                  and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-                  <ul>
-                    <li>It is a long established fact that a reader will be distracted</li>
-                    <li>software like Aldus PageMaker including versions of Lorem Ipsum</li>
-                    <li>The standard chunk of Lorem Ipsum used</li>
-                  </ul>
-            </div>
+      <div className="exp-box-wrapper">
+        <div className="exp-box-container">
+          <h1>GLOBAL TECH</h1>
+          <p>Date 2019-3-10</p>
+          <p>
+            I have had the opportunity to create a company portfolio website,
+            where my primary responsibilities included designing and developing
+            the website.
+          </p>
         </div>
+        {/* --------------- */}
+        <div className="exp-box-container">
+          <h1>SMARTDOORS</h1>
+          <p>Date 2022-3-10</p>
+          <p>
+            I have developed a website for SmartDoors company, which includes an
+            about page, home page, and product page. The website is designed to
+            showcase the company's products and services in a user friendly
+            manner.
+          </p>
+        </div>
+        {/* ---------------- */}
+        <div className="exp-box-container">
+          <h1>NAKSAL GUMPA</h1>
+          <p>Date DATE 2023-3-10</p>
+          <p>
+            I have designed a website for the Naksal Monastery, featuring a
+            banner, sections for information about the monastery, its history, a
+            gallery of images, news updates, and a contact page. My primary role
+            in this project was to develop the API and assist with frontend
+            development.
+          </p>
+        </div>
+
+        <div className="exp-box-container">
+          <h1>PADMAI SCHOOL</h1>
+          <p>Date DATE 2023-6-5</p>
+          <p>
+            I have developed a website for a non funded school, which includes
+            features such as a banner, about section, history, gallery, news,
+            and contact information. My role in this project was to build the
+            API and create the user interface. I worked as a full stack
+            developer, handling both front end and back-end development tasks.
+          </p>
+        </div>
+
+        <div className="exp-box-container">
+          <h1>PHULARYI gUMPA</h1>
+          <p>Date 2024-2-8</p>
+          <p>
+            I have developed a web app for Phulari Monastery located in Boudha,
+            which serves as a prayer ordering system. The features of the app
+            include adding sponsor details, ordering pujas, implementing a
+            filter system, and providing a dashboard for the administrative
+            team. My role in this project was to develop the API and handle the
+            frontend API.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ExperiencePage
+export default ExperiencePage;
